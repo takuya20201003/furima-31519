809b77e523a6dadb7de31f2ee84f2e50ab47caa5
takuya20201003 <takuya@MacBook-Air.local>
2021-02-23 20:43:42 +0900

    モデルの単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { 'test12' }
+    password_confirmation { password }
+  end
+end
