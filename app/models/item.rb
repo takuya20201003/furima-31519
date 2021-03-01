ad9c151b61b437fdf3252c5123661c358d82bc7f
takuya20201003 <takuya@MacBook-Air.local>
2021-03-01 22:04:15 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,22 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, completed_at, condition, prefecture, shipping_charge
+
+  with_options presence: true do
+    validates :name
+    validates :description
+  end
+
+  with_options presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} do
+    validates :price
+  end
+
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :completed_at_id
+  end
+
 end
