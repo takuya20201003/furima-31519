04cd7dc78616130d2949ffcd52616df48a347d2b
takuya20201003 <takuya@MacBook-Air.local>
2021-03-01 20:07:33 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, completed_at, condition, prefecture, shipping_charge
 end
