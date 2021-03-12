ccba920a85e53e26a824e7a2ca34600bfc17002c
takuya20201003 <takuya@MacBook-Air.local>
2021-03-12 18:41:12 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_root_path, only: [:edit, :update]
+  before_action :move_to_root_path, only: [:edit, :update, :destory]
   
 
 
