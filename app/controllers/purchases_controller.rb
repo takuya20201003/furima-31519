3736d88b756aaa4f6f79e136106cfce4173de03a
takuya20201003 <takuya@MacBook-Air.local>
2021-03-24 15:34:02 +0900

    before actionの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -38,7 +38,10 @@ class PurchasesController < ApplicationController
   end
 
   def move_to_root_path
-    if current_user == @item.user or @item.purchase
+    if current_user == @item.user
       redirect_to root_path
+    elsif @item.purchase
+      redirect_to root_path
+    end
   end
 end
